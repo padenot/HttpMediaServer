d948aa614243f28b27cd0741a8e5c2e6dc810eb8
Chris Pearce <chris@pearce.org.nz>
2011-11-15 16:00:29 +1300

    Print out host IP when loading.

--- Sockets.cpp
+++ Sockets.cpp
@@ -169,6 +169,11 @@ int Socket::Shutdown() {
 #include <unistd.h>
 #include <netinet/in.h>
 #include <sys/select.h>
+#include <sys/socket.h>
+#include <netdb.h>
+#include <arpa/inet.h>
+
+#define SOCKET_ERROR -1
 
 class UnixSocket : public Socket {
 public:
@@ -182,12 +187,19 @@ public:
 
 Socket* Socket::Open(int aPort) {
   int sockfd;
+  int flags = 1;
   struct sockaddr_in serv_addr;
   sockfd = socket(AF_INET, SOCK_STREAM, 0);
   if (sockfd < 0) {
     perror("ERROR opening socket");
     return 0;
   }
+
+  if(setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &flags, sizeof(int))) {
+    perror("Can't set SO_REUSEADDR.");
+    return 0;
+  }
+
   bzero((char *) &serv_addr, sizeof(serv_addr));
   serv_addr.sin_family = AF_INET;
   serv_addr.sin_addr.s_addr = htonl(INADDR_ANY);
@@ -273,4 +285,20 @@ bool Socket::WaitForRead(unsigned timeout) {
   to.tv_usec = timeout;
   
   return select(mSocket + 1, &socks, 0, 0, &to) > 0;
+}
+
+string Socket::GetIP() const {
+  char buf[1024];
+  if (gethostname(buf, sizeof(buf)) == SOCKET_ERROR) {
+    return "Unknown";
+  }
+
+  struct hostent *host = gethostbyname(buf);
+  if(host == NULL) {
+    return "Unknown";
+  }
+
+  char* localIP;
+  localIP = inet_ntoa(*(struct in_addr *)*host->h_addr_list);
+  return string(localIP);
 }
